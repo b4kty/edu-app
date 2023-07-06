ed078564b7586fe3909feb97f2ea2e9864ba3274
Abdul Faqih <abdulfaqih7777@gmail.com>
2023-07-06 11:26:35 +0700

    add source code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(45 212 191)',
+      }
+    },
+  },
+  plugins: [],
+}
+
